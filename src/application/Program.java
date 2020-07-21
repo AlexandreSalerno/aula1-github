6db030c95570f5f401ebd4b823100f2d9ba99068
Alexandre Salerno <alexandre.salerno26@gmail.com>
2020-07-21 15:46:58 -0300

    Mensagem de teste substituida por mensagem de bom dia, acrescentada mensagem de boa tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,8 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
-
+		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
 	}
 
 }
